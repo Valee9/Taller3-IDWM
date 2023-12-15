7d59f7214752d646beeec3606980088604ae7fac
Valee9 <valentina.hormazabal@alumnos.ucn.cl>
2023-12-15 05:45:45 -0300

    feat: Controller admin, create and edit

--- Backend/controllers/admin.js
+++ Backend/controllers/admin.js
@@ -1,5 +1,6 @@
 // Importa el modelo 'Admin' que representa la estructura de datos para los clientes.
 import Admin from '../models/admin.js';
+import bcrypt from 'bcrypt';
 
 /**
  * Obtiene el admin almacenado en la base de datos.
@@ -25,11 +26,18 @@ export const getAdmin = async (req, res) => {
 
 export const createAdmin = async (req, res) => {
     // Obtiene los datos del nuevo admin del cuerpo de la solicitud.
-    const admin = req.body;
+    const { email, name, year, rut } = req.body;
 
     try {
+        // Verifica si se está intentando crear un admin sin email, nombre, año o rut.
+        if (!email || !name || !year || !rut) {
+            return res.status(400).json({ created: false, message: 'Faltan datos obligatorios.' });
+          }
+        const salt = await bcrypt.genSalt();
+          // Genera el hash de la contraseña utilizando el algoritmo bcrypt.
+        const password = await bcrypt.hash(rut,salt);
         // Crea un nuevo admin en la base de datos utilizando el modelo 'Admin'.
-        const newAdmin = await Admin.create(admin);
+        const newAdmin = await Admin.create({ email, name, year, rut, password });
         // Envía una respuesta indicando que el admin se creó con éxito.
         res.status(201).json({ created: true, admin: newAdmin });
     } catch (error) {
@@ -37,3 +45,44 @@ export const createAdmin = async (req, res) => {
         res.status(400).json({ created: false, message: error.message });
     }
 }
+
+export const getAdminByRut = async (req, res) => {
+  const { rut } = req.params;
+  
+  try {
+    // Busca el administrador en la base de datos por rut.
+    const admin = await Admin.findOne({ rut: rut });
+  
+    // Verifica si el administrador fue encontrado.
+    if (!admin) {
+      return res.status(404).json({ message: "Admin not found" });
+    }
+  
+    // Envía una respuesta con la información del administrador.
+    res.status(200).json(admin);
+  } catch (error) {
+    // Envía una respuesta de error si ocurre algún problema durante la búsqueda del administrador.
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const updateAdminByRut = async (req, res) => {
+  const { rut } = req.params;
+  const updates = req.body;
+  
+  try {
+    // Busca y actualiza el administrador en la base de datos.
+    const updatedAdmin = await Admin.findOneAndUpdate({ rut: rut }, updates, { new: true });
+  
+    // Verifica si el administrador fue encontrado y actualizado con éxito.
+    if (!updatedAdmin) {
+      return res.status(404).json({ message: "Admin not updated" });
+    }
+  
+    // Envía una respuesta indicando que el administrador se actualizó con éxito.
+    res.status(200).json({ message: "Admin actualizado correctamente" , continue: true });
+  } catch (error) {
+    // Envía una respuesta de error si ocurre algún problema durante la actualización del administrador.
+    res.status(400).json({ message: error.message });
+  }
+};
