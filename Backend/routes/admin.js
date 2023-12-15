4c2443b562e8ccdb73ee07ea73e88cc3f422127d
Valee9 <valentina.hormazabal@alumnos.ucn.cl>
2023-12-15 05:46:18 -0300

    feat: Add routes in admin

--- Backend/routes/admin.js
+++ Backend/routes/admin.js
@@ -3,12 +3,18 @@
  * los controladores del admin.
  */
 import express from 'express';
-import { getAdmin, createAdmin } from '../controllers/admin.js';
+import { getAdmin, createAdmin, getAdminByRut, updateAdminByRut } from '../controllers/admin.js';
 // El enrutador para las rutas
 const router = express.Router();
 
 // Ruta para obtener los admin
 router.get('/', getAdmin);
+
+// Ruta para obtener un admin por su rut
+router.get('/:rut', getAdminByRut);
+
+// Ruta para actualizar un admin por su rut
+router.put('/:rut', updateAdminByRut);
 
 // Ruta para crear un nuevo admin
 router.post('/', createAdmin);
