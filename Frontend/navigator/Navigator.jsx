dc9d2e344e9b2c707e909c5b98ce462d23bd4e49
Valee9 <valentina.hormazabal@alumnos.ucn.cl>
2023-12-17 19:40:55 -0300

    docs: Component Navigator documentation

--- Frontend/navigator/Navigator.jsx
+++ Frontend/navigator/Navigator.jsx
@@ -7,10 +7,11 @@ import Home from '../components/Home';
 import Profile from '../components/Profile';
 import Password from '../components/Password';
 import { AuthContext } from '../context/AuthContext';
-
+// Crea un Stack Navigator usando @react-navigation/native-stack
 const Stack = createNativeStackNavigator();
-
+// Componente Navigator que gestiona la navegación de la aplicación
 export const Navigator = () => {
+  // Obtiene el estado de autenticación del contexto
   const { status } = useContext(AuthContext);
   console.log(status)
 
@@ -21,12 +22,14 @@ export const Navigator = () => {
       }}
     >
       {status !== 'authenticated' ? (
+         // Pantallas para usuarios no autenticados
         <>
             <Stack.Screen name="Index" component={Index} options={{ headerShown: false }} />
             <Stack.Screen name="Registration" component={Registration} options={{ headerShown: false }} />
             <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
         </>
       ) : (
+         // Pantallas para usuarios autenticados
         <>
             <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
             <Stack.Screen name="Profile" component={Profile} options={{ headerShown: false }} />
