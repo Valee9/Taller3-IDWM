4884c48bad8b690ef22b56b825458e02e81ae559
Valee9 <valentina.hormazabal@alumnos.ucn.cl>
2023-12-17 19:39:24 -0300

    docs: Component Auth documentation

--- Frontend/context/AuthContext.jsx
+++ Frontend/context/AuthContext.jsx
@@ -1,3 +1,4 @@
+// Importa react, AuthReducer y axios
 import React, { createContext, useReducer } from 'react'
 import { AuthReducer } from './AuthReducer';
 import axios from "axios";
@@ -16,14 +17,14 @@ export const AuthContext = createContext();
 
 // Crear Provider
 export const AuthProvider = ({ children }) => {
-
+  // Utiliza el reducer para manejar el estado de autenticación
   const [state, dispatch] = useReducer(AuthReducer, authInitialState);
-
+  // Función para realizar el registro de un nuevo administrador  
   const signUp = async ({ email, name, year, rut }) => {
-    console.log(email, name, year, rut);
     try {
+      // Realiza una solicitud al servidor para registrar un nuevo administrador
       const response = await axios.post('http://localhost:3001/admin', { email, name, year, rut });
-      console.log(response.created);
+      // Actualiza el estado después de un registro exitoso
       dispatch({
         type: 'checking',
         payload: {
@@ -32,25 +33,26 @@ export const AuthProvider = ({ children }) => {
           rut: response.data.rut,
         }
       });
+      // Retorna un objeto con éxito y datos
       return { success: true, data: response.data };
     } catch (error) {
-      console.log(error.response);
+      // Actualiza el estado con un mensaje de error en caso de fallo en el registro
       dispatch({
         type: 'setError',
         payload: {
           errorMessage: 'Hubo un error en el registro.'
         }
       });
+      // Retorna un objeto con fallo y mensaje de error
       return { success: false, error: 'Hubo un error en el registro.' };
     }
   };
-
-
+  // Función para realizar el inicio de sesión
   const signIn = async ({ email, password }) => {
     try {
+      // Realiza una solicitud al servidor para iniciar sesión
       const response = await axios.post('http://localhost:3001/auth', { email, password });
-      console.log(response.data.token);
-      console.log(response.data.admin);
+      // Actualiza el estado después de un inicio de sesión exitoso
       dispatch({
         type: 'signUp',
         payload: {
@@ -59,31 +61,35 @@ export const AuthProvider = ({ children }) => {
           rut: response.data.admin.rut,
         }
       });
+       // Retorna un objeto con éxito y datos
       return { success: true, data: response.data };
     } catch (error) {
-      console.log(error.response);
+      // Actualiza el estado con un mensaje de error en caso de fallo en el inicio de sesión
       dispatch({
         type: 'setError',
         payload: {
           errorMessage: 'Hubo un error en el inicio de sesión.'
         }
       });
+      // Retorna un objeto con fallo y mensaje de error
       return { success: false, error: 'Hubo un error en el inicio de sesión.' };
     }
   };
-
+  // Función para cerrar sesión
   const logOut = async () => {
+    // Actualiza el estado para realizar el cierre de sesión
     dispatch({
       type: 'logout',
     })
   }
-
+  // Función para remover el mensaje de error
   const removeError = () => {
+    // Actualiza el estado para eliminar el mensaje de error
     dispatch({
       type: 'removeError'
     })
   }
-
+  // Retorna el contexto de autenticación y sus funciones alrededor de los componentes hijos
   return (
     <AuthContext.Provider
       value={{
--- Frontend/context/AuthReducer.jsx
+++ Frontend/context/AuthReducer.jsx
@@ -1,14 +1,6 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-
-// Tipo acciones
-// sign-up
-// addError
-// removeError
-// notAuthenticated
-// logout
 export const AuthReducer = (state, action) => {
     switch (action.type) {
+        // Acción para un registro exitoso
         case "signUp":
             return {
                 ...state,
@@ -18,10 +10,12 @@ export const AuthReducer = (state, action) => {
                 user: action.payload.user,
                 rut: action.payload.rut,
             }
+        // Acción para cuando el usuario no está autenticado
         case "notAuthenticated":
             return {
 
             }
+        // Acción para cerrar sesión
         case "logout":
             return {
                 ...state,
@@ -30,6 +24,7 @@ export const AuthReducer = (state, action) => {
                 token: null,
                 user: null,
             }
+        // Acción para agregar un mensaje de error
         case "addError":
             return {
                 ...state,
@@ -39,11 +34,13 @@ export const AuthReducer = (state, action) => {
                 user: null,
                 rut: null
             }
+        // Acción para eliminar el mensaje de error
         case "removeError":
             return {
                 ...state,
                 errorMessage: [],
             }
+        // Por defecto, retorna el estado actual sin cambios
         default:
             return state;
     }
